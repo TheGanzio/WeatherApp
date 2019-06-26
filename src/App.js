8e42d5fd2e8d5c14fbc6306702d8bf21136c7077
TheGanzio <the0ganzio@gmail.com>
2019-06-26 14:32:23 +0300

    release v1.1

--- src/App.js
+++ src/App.js
@@ -22,39 +22,47 @@ class App extends React.Component {
 
     gettingWeather = async (event) => {
 
-            event.preventDefault();
-            const CITY = event.target.elements.city.value;
+        event.preventDefault();
+        const CITY = event.target.elements.city.value;
 
-            if (CITY) {
+        if (CITY) {
             const apiUrl = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`);
 
             const data = await apiUrl.json();
 
-<<<<<<< HEAD
-            const dateRise = new Date(data.sys.sunrise*1000);
-=======
-            let dateRise = new Date(data.sys.sunrise*1000);
->>>>>>> 31c85f2f1be59cae40baa6aa00a46210a55f0b4f
-            const hoursRise = dateRise.getHours();
-            const minutesRise = "0" + dateRise.getMinutes();
-            const secondsRise = "0" + dateRise.getSeconds();
-            const sunrise_date = hoursRise + ':' + minutesRise.substr(-2) + ':' + secondsRise.substr(-2);
+            if (data.sys === undefined) {
+                this.setState({
+                    temp: undefined,
+                    city: undefined,
+                    country: undefined,
+                    sunrise: undefined,
+                    sunset: undefined,
+                    error: 'Enter a valid city'
+                });
+            } else if (data.sys !== undefined) {
 
-            const dateSet = new Date(data.sys.sunset*1000);
-            const hoursSet = dateSet.getHours();
-            const minutesSet = "0" + dateSet.getMinutes();
-            const secondsSet = "0" + dateSet.getSeconds();
-            const sunset_date = hoursSet + ':' + minutesSet.substr(-2) + ':' + secondsSet.substr(-2);
 
-            this.setState({
-                temp: data.main.temp,
-                city: data.name,
-                country: data.sys.country,
-                sunrise: sunrise_date,
-                sunset: sunset_date,
-                error: undefined
-            });
-        } else {
+                const dateRise = new Date(data.sys.sunrise * 1000);
+                const hoursRise = dateRise.getHours();
+                const minutesRise = "0" + dateRise.getMinutes();
+                const secondsRise = "0" + dateRise.getSeconds();
+                const sunrise_date = hoursRise + ':' + minutesRise.substr(-2) + ':' + secondsRise.substr(-2);
+
+                const dateSet = new Date(data.sys.sunset * 1000);
+                const hoursSet = dateSet.getHours();
+                const minutesSet = "0" + dateSet.getMinutes();
+                const secondsSet = "0" + dateSet.getSeconds();
+                const sunset_date = hoursSet + ':' + minutesSet.substr(-2) + ':' + secondsSet.substr(-2);
+
+                this.setState({
+                    temp: data.main.temp,
+                    city: data.name,
+                    country: data.sys.country,
+                    sunrise: sunrise_date,
+                    sunset: sunset_date,
+                    error: undefined
+                });
+            } else {
                 this.setState({
                     temp: undefined,
                     city: undefined,
@@ -64,6 +72,7 @@ class App extends React.Component {
                     error: "Enter a valid city name"
                 });
             }
+        }
     };
 
     render() {
